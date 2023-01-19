942be6110bd1cf6f88374e7b8bf1d7f3590ff740
Empty40 <Sorok041@gmail.com>
2023-01-19 09:11:23 +0300

    ТЗ 5, 19.01.23

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -6,7 +6,7 @@ import models.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
